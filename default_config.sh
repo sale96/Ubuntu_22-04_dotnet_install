b4ad2e13d1364ef23ba30d9a513aa3ccd04b9e20
Aleksandar Predojevic <aca801@gmail.com>
2023-04-22 14:03:35 +0200

    Github username and email script run

--- default_config.sh
+++ default_config.sh
@@ -3,3 +3,11 @@
 echo EDITOR=vim >> ~/.bashrc
 git config --global core.editor "vim"
 source ~/.bashrc
+
+echo "Email for github:"
+read email
+echo "Username for github:"
+read username
+
+git config --global user.name $username
+git config --global user.email $email
