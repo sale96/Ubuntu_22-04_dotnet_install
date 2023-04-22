62ed974e6601547fa99c1e3508b0747fba4452ec
Aleksandar Predojevic <aca801@gmail.com>
2023-04-22 14:00:07 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -5,9 +5,11 @@ sudo chmod +x ssh.sh
 sudo chmod +x docker.sh
 sudo chmod +x docker-sudo.sh
 sudo chmod +x runtime.sh
+sudo chmod +x default_config.sh
 
 ./dotnet.sh
 ./ssh.sh
 ./docker.sh
 ./docker-sudo.sh
 ./runtime.sh
+./default_config.sh
